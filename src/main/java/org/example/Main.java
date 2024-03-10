83a70777862d8b4ebc8a8d25571560f6292eab21
Timur-Gus <timurka24478@gmail.ru>
2024-03-10 15:33:42 +0300

    Лист String переделан в лист Integer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 
-import org.example.arrayStringInterface.StringList;
+import org.example.arrayInterface.StringList;
 
 public class Main {
     public static void main(String[] args) {
--- src/test/java/org/example/StringListImplTest.java
+++ src/test/java/org/example/StringListImplTest.java
@@ -1,156 +1,152 @@
 package org.example;
 
-import org.example.arrayStringInterface.StringList;
-import org.example.myException.ElementNotFoundException;
+import org.example.arrayInterface.IntegerList;
 import org.example.myException.IndexIsTooLarge;
 import org.example.myException.ListIsFullException;
 import org.example.myException.NullItemException;
 import org.junit.jupiter.api.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringListImplTest {
-    StringList stringList;
+class IntegerListImplTest {
+    IntegerList IntegerList;
 
     @BeforeEach
     public void init() {
-        this.stringList = new StringListImpl(3);
+        this.IntegerList = new IntegerListImpl(3);
     }
     @Test
     void add() {
-        String actual = stringList.add("Текст");
+        Integer actual = IntegerList.add("Текст");
         assertEquals("Текст", actual);
     }
 
     @Test
     void testAdd() {
-        String actual = stringList.add(0, "Текст");
+        Integer actual = IntegerList.add(0, "Текст");
         assertEquals("Текст", actual);
     }
     @Test
     void addByIndex() {
-        stringList.add(0, "Текст");
-        assertEquals("Текст", stringList.get(0));
+        IntegerList.add(0, "Текст");
+        assertEquals("Текст", IntegerList.get(0));
     }
     @Test
     void addNull() {
 
         NullItemException nullItemException = assertThrows(NullItemException.class,
                 () ->
-                stringList.add(null)
+                IntegerList.add(null)
                 );
     }
     @Test
     void addInFullList() {
-        stringList.add("Текст");
-        stringList.add("Текст");
-        stringList.add("Текст");
+        IntegerList.add("Текст");
+        IntegerList.add("Текст");
+        IntegerList.add("Текст");
         ListIsFullException listIsFullException = assertThrows(ListIsFullException.class,
                 () ->
-                stringList.add("Текст")
+                IntegerList.add("Текст")
                 );
     }
     @Test
     void addNullByIndex() {
         NullItemException nullItemException = assertThrows(NullItemException.class,
                 () ->
-                stringList.add(0,null)
+                IntegerList.add(0,null)
                 );
     }
     @Test
     void addInFullListByIndex() {
-        stringList.add("Текст");
-        stringList.add("Текст");
-        stringList.add("Текст");
+        IntegerList.add("Текст");
+        IntegerList.add("Текст");
+        IntegerList.add("Текст");
         ListIsFullException listIsFullException = assertThrows(ListIsFullException.class,
                 () -> {
-                stringList.add(0,"Текст");
+                IntegerList.add(0,"Текст");
         });
     }
 
     @Test
     void addByLargeIndex() {
         IndexIsTooLarge indexIsTooLarge = assertThrows(IndexIsTooLarge.class, () -> {
-            stringList.add(5, "Text");
+            IntegerList.add(5, "Text");
         });
     }
 
     @Test
     void set() {
-        assertEquals("Text",stringList.set(0, "Text"));
+        assertEquals("Text",IntegerList.set(0, "Text"));
     }
 
     @Test
     void remove() {
-        stringList.add("Текст");
-        stringList.add("Текст2");
-        stringList.remove(0);
-        assertEquals("Текст2", stringList.get(0));
+        IntegerList.add("Текст");
+        IntegerList.add("Текст2");
+        IntegerList.remove(0);
+        assertEquals("Текст2", IntegerList.get(0));
     }
 
     @Test
     void testRemove() {
-        stringList.add("Текст");
-        stringList.add("Текст2");
-        stringList.remove("Текст");
-        assertEquals("Текст2", stringList.get(0));
+        IntegerList.add("Текст");
+        IntegerList.add("Текст2");
+        IntegerList.remove("Текст");
+        assertEquals("Текст2", IntegerList.get(0));
     }
 
     @Test
     void contains() {
-        stringList.add("Текст");
-        assertTrue(stringList.contains("Текст"));
+        IntegerList.add("Текст");
+        assertTrue(IntegerList.contains("Текст"));
     }
     @Test
     void containsFalse() {
-        stringList.add("Текст");
-        assertFalse(stringList.contains("Текст2"));
+        IntegerList.add("Текст");
+        assertFalse(IntegerList.contains("Текст2"));
     }
 
     @Test
     void indexOf() {
-        stringList.add("Text");
-        stringList.add("Text2");
-        stringList.add("Text3");
-        assertEquals(1, stringList.indexOf("Text2"));
+        IntegerList.add("Text");
+        IntegerList.add("Text2");
+        IntegerList.add("Text3");
+        assertEquals(1, IntegerList.indexOf("Text2"));
     }
 
     @Test
     void lastIndexOf() {
-        stringList.add("Text");
-        stringList.add("Text2");
-        stringList.add("Text3");
-        assertEquals(1, stringList.indexOf("Text2"));
+        IntegerList.add("Text");
+        IntegerList.add("Text2");
+        IntegerList.add("Text3");
+        assertEquals(1, IntegerList.indexOf("Text2"));
     }
 
     @Test
     void get() {
-        stringList.add("Text");
-        assertEquals("Text", stringList.get(0));
+        IntegerList.add("Text");
+        assertEquals("Text", IntegerList.get(0));
     }
 
     @Test
     void size() {
-        stringList.add("Text");
-        stringList.add("Text");
-        stringList.add("Text");
-        assertEquals(3, stringList.size());
+        IntegerList.add("Text");
+        IntegerList.add("Text");
+        IntegerList.add("Text");
+        assertEquals(3, IntegerList.size());
     }
 
     @Test
     void isEmpty() {
-        assertTrue(stringList.isEmpty());
+        assertTrue(IntegerList.isEmpty());
     }
 
     @Test
     void clear() {
-        stringList.add("Text");
-        stringList.add("Text");
-        stringList.clear();
-        assertTrue(stringList.isEmpty());
+        IntegerList.add("Text");
+        IntegerList.add("Text");
+        IntegerList.clear();
+        assertTrue(IntegerList.isEmpty());
 
     }
 
